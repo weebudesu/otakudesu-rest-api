3d5429fb1b088f4eb5966c2ff4615403af35a782
weebudesu <114684029+weebudesu@users.noreply.github.com>
2023-04-17 00:47:04 +0700

    Update base-url.js

--- helpers/base-url.js
+++ helpers/base-url.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseUrl: 'https://otakudesu.bid/',
+    baseUrl: 'https://otakudesu.lol/',
     completeAnime:'complete-anime/',
     onGoingAnime:'ongoing-anime/',
     schedule:'jadwal-rilis/',
